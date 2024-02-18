7f31e1ba7cdf9fbc0243e45ebbdeae3afb6bdba9
FIlip Boisen <filip.boisen@elev.ga.lbs.se>
2024-02-18 11:58:33 +0100

    New State

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,11 +7,21 @@ public class Enemy : MonoBehaviour
 {
     private StateMachine stateMachine;
     private NavMeshAgent agent;
+    private GameObject player;
 
     public NavMeshAgent Agent { get => agent; }
+
+    public WalkPath path;
+    [Header("Sight Values")]
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
+    [Header("WeaponValues")]
+    public Transform gunbarrel;
+    [Range(0.1f, 10f)]
+    public float fireRate;
     //Just for debugging purposes.
     [SerializeField] private string currentState;
-    public WalkPath path;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +29,41 @@ public class Enemy : MonoBehaviour
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
+        currentState = stateMachine.activeState.ToString();
+    }
+
+    public bool CanSeePlayer()
+    {
+        if (player != null)
+        {
+            //can the enemy see the player?
+            if (Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if(angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView) 
+                {
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if(Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+                        if(hitInfo.transform.gameObject == player)
+                        {
+                            return true;
+                        }
+                    }
+                    Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                }
+            }
+
+        }
+        return false;
     }
 }
